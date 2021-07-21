38ba49011754a7d0a225138fc056cdae63eefdd4
pau <pau_ubu>
2021-07-20 23:16:05 -0400

    mantener imagenes en local storage

--- 02-gifApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifApp/src/app/gifs/services/gifs.service.ts
@@ -20,6 +20,7 @@ export class GifsService {
     // if(localStorage.getItem('historial')){
     //   this._historial = JSON.parse( localStorage.getItem('historial')!);
     // }
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || []; 
   }
 
   buscarGifs(query: string = '') {
@@ -35,6 +36,7 @@ export class GifsService {
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
   }
 }
