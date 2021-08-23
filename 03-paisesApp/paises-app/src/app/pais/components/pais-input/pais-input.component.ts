18d84d9c7100de2c5c58539099d88f52c13acb29
pau <pau_ubu>
2021-08-22 23:50:51 -0400

    mostrar sugerencias de busqueda por pais

--- 03-paisesApp/paises-app/src/app/pais/components/pais-input/pais-input.component.ts
+++ 03-paisesApp/paises-app/src/app/pais/components/pais-input/pais-input.component.ts
@@ -11,19 +11,23 @@ export class PaisInputComponent implements OnInit {
 
   @Output() onEnter: EventEmitter<string> =  new EventEmitter();
   @Output() onDebounce: EventEmitter<string> =  new EventEmitter();
+  @Input() placeholder: string = '';
   debouncer: Subject<string> = new Subject();
   termino: string = '';
-  @Input() placeholder: string = '';
+  ocultarSugerencias: boolean = true;
+  
 
   ngOnInit() {
     this.debouncer
     .pipe(debounceTime(300))
     .subscribe( valor =>{
       console.log('debouncer: ', valor);
-    })
+      this.onDebounce.emit( valor);
+    });
   }
 
   buscar(){
+    this.ocultarSugerencias =false;
     this.onEnter.emit(this.termino);
   }
 
--- 03-paisesApp/paises-app/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/paises-app/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 import { CountryResponse } from '../../interfaces/countryResponse.interface';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles: [`li {cursor: pointer;}`]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
-  paises: CountryResponse[]=[];
+  paises: CountryResponse[] = [];
   title: string = 'País';
-  
+  paisesSugeridos: CountryResponse[] = [];
+  mostrarSugerencias: boolean = false;
+
   constructor(private paisService: PaisService) {
   }
 
-  buscar( termino: string) {
+  buscar(termino: string) {
     console.log(this.termino);
-    this.termino=termino;
+    this.mostrarSugerencias = false;
+    this.termino = termino;
     this.paisService.buscarPaisPorNombre(this.termino)
       .subscribe((paises) => {
         console.log(paises);
@@ -33,9 +37,16 @@ export class PorPaisComponent {
       );
   }
 
-  sugerencias(termino: string){
-    this.hayError=false;
-    //TODO: crear sugerencias
+  sugerencias(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPaisPorNombre(termino)
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0, 5),
+        (err) => this.paisesSugeridos = []
+      );
   }
 
 }
