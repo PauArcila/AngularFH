2e7077e9069dd27accdc72f39a42fb375bf1f7f1
pau <pau_ubu>
2021-09-18 21:44:47 -0300

    numeros Pipes

--- 04-pipesApp/src/app/ventas/pages/numeros/numeros.component.ts
+++ 04-pipesApp/src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html'
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NumerosComponent  {
+  ventasNetas: number = 24454474.5454;
+  porcentaje: number = 0.19553;
 
 }
