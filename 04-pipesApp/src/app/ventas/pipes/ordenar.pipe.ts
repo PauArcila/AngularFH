01f50a5321412f2596e8aeab47f027831196afde
pau <pau_ubu>
2021-09-25 21:16:04 -0300

    fin de seccion pipes personalizados

--- 04-pipesApp/src/app/ventas/pipes/ordenar.pipe.ts
+++ 04-pipesApp/src/app/ventas/pipes/ordenar.pipe.ts
@@ -11,13 +11,13 @@ export class OrdenarPipe implements PipeTransform {
     switch (orderBy) {
 
       case 'nombre':
-        return heroes = heroes.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1);
+        return heroes.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1);
 
       case 'vuela':
-        return heroes = heroes.sort((a, b) => (a.vuela > b.vuela) ? -1 : 1);
+        return heroes.sort((a, b) => (a.vuela > b.vuela) ? -1 : 1);
 
       case 'color':
-        return heroes = heroes.sort((a, b) => (a.color > b.color) ? 1 : -1);
+        return heroes.sort((a, b) => (a.color > b.color) ? 1 : -1);
 
       default:
         return heroes;
