41aeaaf1bc48cf4d11b933446523401063074277
Pau Arcila <pvenegaa@everis.com>
2021-09-28 12:06:34 -0300

    incorporo elementos de material design

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
--- 05-heroesApp/src/app/heroes/pages/home/home.component.ts
+++ 05-heroesApp/src/app/heroes/pages/home/home.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
+  styles: [`.container {
+    margin: 1rem;
+  }`
   ]
 })
 export class HomeComponent implements OnInit {
