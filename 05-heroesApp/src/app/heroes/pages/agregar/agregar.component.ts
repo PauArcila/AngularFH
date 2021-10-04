7f05aa31c2792473f10035e4c4291591f8ca54e0
Pau Arcila <pvenegaa@everis.com>
2021-10-04 12:53:33 -0300

    formulario agregar superheroe

--- 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -8,6 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
+  publishers = [
+    { id: Publisher.DCComics, desc: 'DC - Comics'},
+    { id: Publisher.MarvelComics, desc: 'Marvel - Comics'}
+  ]
+
+  heroe: Heroe = {
+    id: '',
+    superhero: '',
+    publisher: Publisher.MarvelComics,
+    alter_ego: '',
+    first_appearance: '',
+    characters: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
